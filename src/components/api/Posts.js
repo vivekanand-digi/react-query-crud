4e41e2c3efa8a734fa19790a715d69d8b7db2555
vivekanand-digi <117620313+vivekanand-digi@users.noreply.github.com>
2023-10-03 10:04:34 +0530

    implemented axios

--- src/components/api/Posts.js
+++ src/components/api/Posts.js
@@ -1,37 +1,33 @@
+import axios from "axios";
+
 export const addTodo = async (newTodo) => {
-    await fetch("http://localhost:3000/posts", {
-      method: "POST",
+    await axios.post("http://localhost:3000/posts",newTodo, {
       headers: {
         "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newTodo),
+      }
     });
   };
 
   export const fetchTodos = async () => {
-    const res = await fetch('http://localhost:3000/posts');
-    return res.json();
+    const res = await axios.get('http://localhost:3000/posts');
+    return res.data
   };
   
   export const deleteTodo = async (id) => {
-    const response = await fetch(`http://localhost:3000/posts/${id}`, {
-      method: 'DELETE',
-    });
-    return response.json();
+    const response = await axios.delete(`http://localhost:3000/posts/${id}`);
+    return response.data
   };
 
   export const fetchTodo = async (id) => {
-    const res = await fetch(`http://localhost:3000/posts/${id}`);
-    return res.json();
+    const res = await axios.get(`http://localhost:3000/posts/${id}`);
+    return res.data
   };
 
   export const updateTodo = async (updatedTodo) => {
-    await fetch(`http://localhost:3000/posts/${updatedTodo.id}`, {
-      method: "PUT",
+    await axios.put(`http://localhost:3000/posts/${updatedTodo.id}`, updatedTodo, {
       headers: {
         "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updatedTodo),
+      }
     });
   };
   
